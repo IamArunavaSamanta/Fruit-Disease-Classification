bd6277679b4b6d8e15a1abc9a7209b22fae210e6
Arunava Samanta <arunavasamanta001@gmail.com>
2025-11-25 22:52:26 +0530

    Update and rename StreamlitVehicle.py to App.py

---
+++ App.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+import keras
+
+st.image("https://github.com/IamArunavaSamanta/CNN-Streamlit-Vehicles-Classification-Project/blob/main/images/All_Vehicles.png?raw=true", width=400)
+st.markdown('''#### :red-background[:orange[CNN]]:orange[, or Convolutional Neural Network,] ''')
+    
+         
