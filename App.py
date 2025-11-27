df47fc76eaeb38fb08010db45c17cdb87b4a038f
Arunava Samanta <arunavasamanta001@gmail.com>
2025-11-27 20:28:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,5 +6,6 @@ import sklearn
 import keras
 
 st.set_page_config(layout="wide")
-st.image("https://github.com/IamArunavaSamanta/CNN-Streamlit-Vehicles-Classification-Project/blob/main/images/All_Vehicles.png?raw=true", width=400)
+st.image("https://raw.githubusercontent.com/IamArunavaSamanta/Fruit-Disease-Classification/main/images/Fruits%20Image.png", width=400)
 st.markdown('''#### :red-background[:orange[CNN]]:orange[, or Convolutional Neural Network,] ''')
+
