e4947b659fc177c33370608e3f4e195f2a5690ba
Arunava Samanta <arunavasamanta001@gmail.com>
2025-11-27 20:31:03 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,6 +6,7 @@ import sklearn
 import keras
 
 st.set_page_config(layout="wide")
-st.image("https://raw.githubusercontent.com/IamArunavaSamanta/Fruit-Disease-Classification/main/images/Fruits%20Image.png", width=400)
+st.image("https://raw.githubusercontent.com/IamArunavaSamanta/Fruit-Disease-Classification/main/images/Fruits%20Image.png", width=600)
 st.markdown('''#### :red-background[:orange[CNN]]:orange[, or Convolutional Neural Network,] ''')
+
 
